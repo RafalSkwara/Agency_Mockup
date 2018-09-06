5a2f452ab0e8b1cd6af9664cb14665c5aa32705b
Rafał Skwara <RSkwara@alfa.wsip.com.pl>
2018-09-06 15:53:53 +0200

    early version of menu added

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
     let Model = function() {
         this.servicesTop = 0;
-        this.scroll = 0;
+		this.scroll = 0;
+		
+		this.menuVisible = false;
     }
 
     let View = function(model) {
+		//-- menu related selectors
+		this.SECTIONS = document.querySelectorAll("section");
+		this.MENU = document.querySelector('.menu');
+		this.MENU_TOGGLE_MAIN = document.querySelector('.menu-toggle--main');
+		this.MENU_TOGGLE = document.querySelectorAll('.menu-toggle');
+		this.MENU_BTNS = document.querySelectorAll(".menu__item");
+		
+		//-- back-to-top related selectors
         this.ARROW = document.querySelector('.arrow');
         this.HEADER = document.querySelector('header');
         this.SECTION_SERVICES = document.querySelector('section.services');
 		this.TO_TOP = document.querySelector('.back-to-top');
 		
+		//-- carousel selectors
 		this.CAROUSEL = document.querySelector('.carousel');
 		this.CAROUSEL_PREV = document.querySelector('#prev');
 		this.CAROUSEL_NEXT = document.querySelector('#next');
     }
-
+	
     let Controller = function(model, view) {
+		//-- helper functions
+		this.moveToTop = () => {
+			model.scroll = 0;
+			window.scroll({
+				top: model.scroll,
+				left: 0,
+				behavior: 'smooth'
+			});
+		};
+		
+		this.menuHandler = () => {
+			if (model.menuVisible) {
+				view.MENU.classList.remove('visible');
+				model.menuVisible = false;
+				console.log(view.MENU_TOGGLE_MAIN.style.display);
+				
+				view.MENU_TOGGLE_MAIN.classList.add('visible')
+			} else {
+				view.MENU.classList.add('visible');
+				model.menuVisible = true;
+				view.MENU_TOGGLE_MAIN.classList.remove('visible')
+			}
+		};
+
+		//-- handler functions
         this.arrowHandler = () => {
             model.servicesTop = view.SECTION_SERVICES.offsetTop;
             window.scroll({
@@ -23,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 left: 0,
                 behavior: 'smooth'
             });
-        }
+        };
 
         this.scrollHandler = () => {
             model.scroll = window.scrollY;
@@ -32,24 +68,37 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 view.TO_TOP.classList.remove('visible');
             }
-        }
-
-        this.moveToTop = () => {
-            model.scroll = 0;
-            window.scroll({
-                top: model.scroll,
-                left: 0,
-                behavior: 'smooth'
-            });
-        }
+        };
 
 
-        //event listeners
+		this.menuClickHandler = (e) => {
+			e.preventDefault();
+			let data = e.target.dataset.js;
+			let el = [...view.SECTIONS].filter(el => el.classList.contains(data));
+			console.log(el);
+			let offset = el[0].offsetTop;
+			window.scroll({
+				top: offset,
+				left: 0,
+				behavior: 'smooth'
+			});
+		};
+
+		//event listeners
+		
+		//listener for menu items
+		view.MENU_BTNS.forEach(element => {
+			element.addEventListener('click', (e) => this.menuClickHandler(e));
+		});
+
+		//listener for menu toggling
+		view.MENU_TOGGLE.forEach(el => el.addEventListener('click', this.menuHandler));
 
         view.ARROW.addEventListener('click', this.arrowHandler);
         window.addEventListener('scroll', this.scrollHandler);
 		view.TO_TOP.addEventListener('click', this.moveToTop);
 
+		
 		
 		//libraries initialisation
 		let flkty = new Flickity(view.CAROUSEL, {
